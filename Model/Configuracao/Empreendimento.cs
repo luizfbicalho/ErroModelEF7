4f9b5851644a34072de16b638dfd2d4227fc28b2
Luiz Fernando Bicalho <luiz.bicalho@aglsolutions.com.br>
2023-05-02 14:36:03 -0300

    limpeza

--- Model/Configuracao/Empreendimento.cs
+++ Model/Configuracao/Empreendimento.cs
@@ -14,30 +14,10 @@ namespace Model.Configuracao
     //[DataContract]
     public class Empreendimento:Autorizacao
     {
-        //[StringLength(100)]
-        //[DataMember]
-        //public string? Codigo { get; set; }
-        //[DataMember]
 
-        //public bool ChatAssessoria { get; set; }
-        ////[DataMember]
-        ////public virtual Passo? PassoAssessoria { get; set; }
-        //[DataMember]
-        //public bool ChatCorretor { get; set; }
-        ////[DataMember]
-        ////public virtual Passo? PassoCorretor { get; set; }
-        //[DataMember]
-        //public bool LiberarAcessoCliente { get; set; }
 
         [DataMember]
         public virtual Praca? Praca { get; set; }
 
-        //[DataMember]
-        //[SubForm]
-        //public virtual IList<EmpreendimentoAssessoria> Assessorias { get; set; } = new List<EmpreendimentoAssessoria>();
-        //[Newtonsoft.Json.JsonIgnore]
-        //public virtual IList<Indicador> Indicadores { get; set; } = new List<Indicador>();  
-        //[DateOnly]
-        //public DateTime? DataLancamento { get; set; }
     }
 }
