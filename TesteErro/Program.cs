43f9618da1b9dcd0a89dbb92ce54e91210bd5f59
Luiz Fernando Bicalho <luiz.bicalho@aglsolutions.com.br>
2023-05-03 18:17:26 -0300

    update deleet and create

--- TesteErro/Program.cs
+++ TesteErro/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Model.Configuracao;
 using Model.Models;
 
 namespace TesteErro
@@ -8,9 +9,21 @@ namespace TesteErro
     {
         static void Main(string[] args)
         {
-            var str = "Server=.\\SQL2019;Initial Catalog=sadi;Persist Security Info=False;User ID=sa;Password=sa!2019;MultipleActiveResultSets=True;Encrypt=False;TrustServerCertificate=False;Connection Timeout=30;";
+            var str = "Server=.\\SQL2019;Initial Catalog=sadi2;Persist Security Info=False;User ID=sa;Password=sa!2019;MultipleActiveResultSets=True;Encrypt=False;TrustServerCertificate=False;Connection Timeout=30;";
             var optionsBuilder = new DbContextOptionsBuilder<ModelContext>().UseLazyLoadingProxies()
     .UseSqlServer(str);
+
+            using (var context = new ModelContext(optionsBuilder.Options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                var emp = new Empreendimento { Praca = new Praca() };
+                context.Add(emp);
+                context.Add(emp.Praca);
+                context.SaveChanges();
+            }
+
             using (var context = new ModelContext(optionsBuilder.Options))
             {
                 var emps = context.Empreendimento.ToList();
